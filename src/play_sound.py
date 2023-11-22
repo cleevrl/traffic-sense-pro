80003b4485324831e9d53fc4318e0e788188e63f
cleevrl <cleevrl@gmail.com>
2023-11-22 15:15:23 +0900

    init: init project

--- src/play_sound.py
+++ src/play_sound.py
@@ -7,7 +7,7 @@ def play_sound():
     if not pygame.mixer.music.get_busy():
 
         print("Play!")
-        pygame.mixer.music.load('data/voice/turn_around.mp3')
+        pygame.mixer.music.load('data/voice/turn_around_with_siren.mp3')
         pygame.mixer.music.play()
     
     else:
